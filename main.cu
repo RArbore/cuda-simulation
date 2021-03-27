299c8a4be680426a1105560d56f151080323a70c
RArbore <russel.jma@gmail.com>
2021-03-27 11:15:12 -0700

    Created CUDA simulation

--- main.cu
+++ main.cu
@@ -1,93 +1,159 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 
-#include <GL/gl.h>
-#include <GL/glut.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
-#include <cuda_runtime_api.h>
-#include <cuda_gl_interop.h>
+#define WIDTH 1000
+#define HEIGHT 1000
+#define NUM_AGENTS 10000
+#define AGENTS_ARRAY_SIZE NUM_AGENTS * 3
+#define PIXELS WIDTH * HEIGHT
+#define TOTAL_STEPS 1000000
+#define KERNEL_R 1
+#define DECAY 0.97
+#define SAMPLE_DIST 2
+#define SAMPLE_ANGLE M_PI / 6.0f
+#define TURN_SPEED 0.1
 
-#define WIDTH 800
-#define HEIGHT 600
-#define FOV 45
-#define Z_NEAR 1.0f
-#define Z_FAR 500.0f
+__global__
+void update_agents (float *agents, uint8_t *image) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < NUM_AGENTS) {
+        float x = agents[3 * i];
+        float y = agents[3 * i + 1];
+        float angle = agents[3 * i + 2];
 
-#define KEY_ESC 27
+        float sense_left = image[((int) (x + SAMPLE_DIST * cos(angle + SAMPLE_ANGLE))) + ((int) (y + SAMPLE_DIST * sin(angle + SAMPLE_ANGLE))) * WIDTH];
+        float sense_center = image[((int) (x + SAMPLE_DIST * cos(angle))) + ((int) (y + SAMPLE_DIST * sin(angle))) * WIDTH];
+        float sense_right = image[((int) (x + SAMPLE_DIST * cos(angle - SAMPLE_ANGLE))) + ((int) (y + SAMPLE_DIST * sin(angle - SAMPLE_ANGLE))) * WIDTH];
 
-int count = 0;
+        uint state = (uint) (x * y * angle + sense_left + sense_center + sense_right);
+        state ^= 2747636419u;
+        state *= 2654435769u;
+        state ^= state >> 16;
+        state *= 2654435769u;
+        state ^= state >> 16;
+        state *= 2654435769u;
 
-void initialize () {
-    unsigned char tex_data[64*3];
-    for (int i = 0; i < 64*3; ++i) {
-        tex_data[i] = i;
+        float turn_stren = ((float) (state % 1000)) / 1000.0f;
+
+        if (sense_center < sense_left && sense_center < sense_right) {
+            angle += 2.0f * (turn_stren - 0.5f) * TURN_SPEED;
+        }
+        else if (sense_left > sense_right) {
+            angle += turn_stren * TURN_SPEED;
+        }
+        else {
+            angle -= turn_stren * TURN_SPEED;
+        }
+
+        float vx = cos(angle);
+        float vy = sin(angle);
+
+        x += vx;
+        y += vy;
+
+        if (x < 0) {
+            x = 0;
+            angle = M_PI - angle;
+        }
+        if (y < 0) {
+            y = 0;
+            angle *= -1;
+        }
+        if (x >= WIDTH) {
+            x = WIDTH - 1;
+            angle = M_PI - angle;
+        }
+        if (y >= HEIGHT) {
+            y = HEIGHT - 1;
+            angle *= -1;
+        }
+
+        agents[3 * i] = x;
+        agents[3 * i + 1] = y;
+        agents[3 * i + 2] = angle;
+
+        image[((int) x) + ((int) y) * WIDTH] = 255;
     }
-
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 8, 8, 0, GL_RGB, GL_UNSIGNED_BYTE, tex_data);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(0, WIDTH, 0, HEIGHT, -1.0f, 1.0f);
-    glMatrixMode(GL_MODELVIEW);
-
 }
 
-void display () {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBindTexture(GL_TEXTURE_2D, 1);
-
-    unsigned char tex_data[64*3];
-    for (int i = 0; i < 64*3; ++i) {
-        tex_data[i] = (i + count) % 256;
-    }
-
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 8, 8, GL_RGB, GL_UNSIGNED_BYTE, tex_data);
-
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_QUADS);
-
-    glTexCoord2i(0, 0); glVertex2i(0, 0);
-    glTexCoord2i(0, 1); glVertex2i(0, HEIGHT);
-    glTexCoord2i(1, 1); glVertex2i(WIDTH, HEIGHT);
-    glTexCoord2i(1, 0); glVertex2i(WIDTH, 0);
-
-    glEnd();
-
-    glDisable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    glutSwapBuffers();
-    count += 1;
-}
-
-void keyboard (unsigned char key, int mousePositionX, int mousePositionY) {
-    switch (key) {
-        case KEY_ESC:
-            exit(0);
-            break;
-        default:
-            break;
+__global__
+void update_image (float *agents, uint8_t *image) {
+    const float KERNEL[3][3] = {{0.005, 0.12, 0.005},
+                                {0.12, 0.5, 0.12},
+                                {0.005, 0.12, 0.005}};
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < PIXELS) {
+        int x = i % WIDTH;
+        int y = i / HEIGHT;
+        float total = 0;
+        int count = 0;
+        int left = x - KERNEL_R;
+        int right = x + KERNEL_R;
+        int up = y - KERNEL_R;
+        int down = y + KERNEL_R;
+        if (left < 0) left = 0;
+        if (right > WIDTH - 1) right = WIDTH - 1;
+        if (up < 0) up = 0;
+        if (down > HEIGHT - 1) down = HEIGHT - 1;
+        for (int ix = left; ix <= right; ix++) {
+            for (int iy = up; iy <= down; iy++) {
+                total += ((float) image[ix + iy * WIDTH]) * KERNEL[iy-y+KERNEL_R][ix-x+KERNEL_R];
+                count++;
+            }
+        }
+        image[i] = (int) (total * DECAY);
     }
 }
 
 int main (int argc, char *argv[]) {
 
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    glutCreateWindow("CUDA Simulation");
-    glutDisplayFunc(display);
-    glutIdleFunc(display);
-    glutKeyboardFunc(keyboard);
-    initialize();
-    glutMainLoop();
+    int i;
+
+    float *host_agents;
+    uint8_t *host_image;
+    host_agents = (float *) malloc(AGENTS_ARRAY_SIZE * sizeof(float));
+    host_image = (uint8_t *) malloc(PIXELS * sizeof(uint8_t));
+
+    float *agents;
+    uint8_t *image;
+
+    cudaMalloc(&agents, AGENTS_ARRAY_SIZE * sizeof(float));
+    cudaMalloc(&image, PIXELS * sizeof(uint8_t));
+
+    float angle;
+    for (i = 0; i < NUM_AGENTS; i++) {
+        host_agents[3 * i] = (float) (rand() % WIDTH);
+        host_agents[3 * i + 1] = (float) (rand() % HEIGHT);
+        angle = (float) (rand() % 360);
+        angle *= M_PI/180.0f;
+        host_agents[3 * i + 2] = angle;
+    }
+
+    for (i = 0; i < PIXELS; i++) {
+        host_image[i] = 0;
+    }
+
+    cudaMemcpy(agents, host_agents, AGENTS_ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(image, host_image, PIXELS * sizeof(uint8_t), cudaMemcpyHostToDevice);
+
+    for (i = 0; i < TOTAL_STEPS; i++) {
+        update_image<<<PIXELS/100, 100>>>(agents, image);
+        update_agents<<<NUM_AGENTS/100, 100>>>(agents, image);
+
+        cudaMemcpy(host_image, image, PIXELS * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+        stbi_write_png("output.png", WIDTH, HEIGHT, 1, host_image, WIDTH * 1);
+
+        if (i % 1000 == 0) printf("%d\n", i);
+    }
+
+    //cudaMemcpy(host_agents, agents, AGENTS_ARRAY_SIZE * sizeof(float), cudaMemcpyDeviceToHost);
+
 
     return 0;
 }
