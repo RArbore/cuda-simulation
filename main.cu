256edd0d4f808e86a2c8e6724a0f37aced4f7a2c
RArbore <russel.jma@gmail.com>
2021-03-27 19:18:03 -0700

    Added RGB support

--- main.cu
+++ main.cu
@@ -13,19 +13,23 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define WIDTH 1920
-#define HEIGHT 1080
-#define NUM_AGENTS 50000
+#define WIDTH 3840
+#define HEIGHT 2160
+#define NUM_AGENTS 100000
 #define AGENTS_ARRAY_SIZE NUM_AGENTS * 3
 #define PIXELS WIDTH * HEIGHT
+#define BYTES_PER_PIXEL 4
+#define PIXELS_SIZE PIXELS * BYTES_PER_PIXEL
+#define AGENTS_DIV 100
+#define PIXEL_DIV 256
 #define TOTAL_STEPS 100000
 #define KERNEL_R 1
-#define DECAY 0.98f
-#define TRAVEL_SPEED 2
-#define SAMPLE_DIST 5.0f
+#define DECAY 0.2f
+#define TRAVEL_SPEED 1
+#define SAMPLE_DIST 4.0f
 #define SAMPLE_ANGLE M_PI / 8.0f
-#define TURN_SPEED 0.4f
-#define RANDOM_STREN 0.0f
+#define TURN_SPEED 1.0f
+#define RANDOM_STREN 1.5f
 #define KEY_ESC 27
 
 float *host_agents;
@@ -52,26 +56,39 @@ void update_agents (float *agents, uint8_t *image) {
         float sample_y_center = y + SAMPLE_DIST * sin(angle);
         float sample_x_right = x + SAMPLE_DIST * cos(angle - SAMPLE_ANGLE);
         float sample_y_right = y + SAMPLE_DIST * sin(angle - SAMPLE_ANGLE);
+        float rsx, rsy;
         
         for (float sx = sample_x_left - KERNEL_R; sx <= sample_x_left + KERNEL_R; sx++) {
             for (float sy = sample_y_left - KERNEL_R; sy <= sample_y_left + KERNEL_R; sy++) {
-                if (sx >= 0  && sy >= 0 && sx < WIDTH && sy < HEIGHT) {
-                    sense_left += (float) image[((int) (sx)) + ((int) (sy)) * WIDTH];
-                }
+                rsx = sx;
+                rsy = sy;
+                while (rsx < 0) rsx += WIDTH;
+                while (rsy < 0) rsy += HEIGHT;
+                while (rsx >= WIDTH) rsx -= WIDTH;
+                while (rsy >= HEIGHT) rsy -= HEIGHT;
+                sense_left += (float) image[BYTES_PER_PIXEL * (((int) (rsx)) + ((int) (rsy)) * WIDTH)];
             }
         }
         for (float sx = sample_x_center - KERNEL_R; sx <= sample_x_center + KERNEL_R; sx++) {
             for (float sy = sample_y_center - KERNEL_R; sy <= sample_y_center + KERNEL_R; sy++) {
-                if (sx >= 0  && sy >= 0 && sx < WIDTH && sy < HEIGHT) {
-                    sense_center += (float) image[((int) (sx)) + ((int) (sy)) * WIDTH];
-                }
+                rsx = sx;
+                rsy = sy;
+                while (rsx < 0) rsx += WIDTH;
+                while (rsy < 0) rsy += HEIGHT;
+                while (rsx >= WIDTH) rsx -= WIDTH;
+                while (rsy >= HEIGHT) rsy -= HEIGHT;
+                sense_center += (float) image[BYTES_PER_PIXEL * (((int) (rsx)) + ((int) (rsy)) * WIDTH)];
             }
         }
         for (float sx = sample_x_right - KERNEL_R; sx <= sample_x_right + KERNEL_R; sx++) {
             for (float sy = sample_y_right - KERNEL_R; sy <= sample_y_right + KERNEL_R; sy++) {
-                if (sx >= 0  && sy >= 0 && sx < WIDTH && sy < HEIGHT) {
-                    sense_right += (float) image[((int) (sx)) + ((int) (sy)) * WIDTH];
-                }
+                rsx = sx;
+                rsy = sy;
+                while (rsx < 0) rsx += WIDTH;
+                while (rsy < 0) rsy += HEIGHT;
+                while (rsx >= WIDTH) rsx -= WIDTH;
+                while (rsy >= HEIGHT) rsy -= HEIGHT;
+                sense_right += (float) image[BYTES_PER_PIXEL * (((int) (rsx)) + ((int) (rsy)) * WIDTH)];
             }
         }
 
@@ -98,63 +115,41 @@ void update_agents (float *agents, uint8_t *image) {
             angle -= pow(turn_stren, RANDOM_STREN) * TURN_SPEED;
         }
 
+        float center_angle = atan((y - HEIGHT / 2) / (x - WIDTH / 2));
+        if (x >= WIDTH / 2) {
+            center_angle -= M_PI;
+        }
+
+        center_angle += M_PI;
+
         float vx = cos(angle);
         float vy = sin(angle);
+
+        float vx_c = cos(center_angle);
+        float vy_c = sin(center_angle);
+
+        //vx = vx + vx_c;
+        //vy = vy + vy_c;
 
         for (int iter = 0; iter < TRAVEL_SPEED; iter++) {
             x += vx;
             y += vy;
 
-            if (x < 0) {
-                x = 0;
-                angle = turn_stren * 2 * M_PI;
-                float vx = cos(angle);
-                float vy = sin(angle);
-            }
-            if (y < 0) {
-                y = 0;
-                angle = turn_stren * 2 * M_PI;
-                float vx = cos(angle);
-                float vy = sin(angle);
-            }
-            if (x >= WIDTH) {
-                x = WIDTH - 1;
-                angle = turn_stren * 2 * M_PI;
-                float vx = cos(angle);
-                float vy = sin(angle);
-            }
-            if (y >= HEIGHT) {
-                y = HEIGHT - 1;
-                angle = turn_stren * 2 * M_PI;
-                float vx = cos(angle);
-                float vy = sin(angle);
-            }
+            while (x < 0) x += WIDTH;
+            while (y < 0) y += HEIGHT;
+            while (x >= WIDTH) x -= WIDTH;
+            while (y >= HEIGHT) y -= HEIGHT;
 
-            image[((int) x) + ((int) y) * WIDTH] = 255;
-        }
-
-        if (x < 0) {
-            x = 0;
-            angle = turn_stren * 2 * M_PI;
-        }
-        if (y < 0) {
-            y = 0;
-            angle = turn_stren * 2 * M_PI;
-        }
-        if (x >= WIDTH) {
-            x = WIDTH - 1;
-            angle = turn_stren * 2 * M_PI;
-        }
-        if (y >= HEIGHT) {
-            y = HEIGHT - 1;
-            angle = turn_stren * 2 * M_PI;
+            image[BYTES_PER_PIXEL * (((int) x) + ((int) y) * WIDTH)] = 255;
         }
 
         agents[3 * i] = x;
         agents[3 * i + 1] = y;
         agents[3 * i + 2] = angle;
 
-        image[((int) x) + ((int) y) * WIDTH] = 255;
+        image[BYTES_PER_PIXEL * (((int) x) + ((int) y) * WIDTH)] = 255;
+        image[BYTES_PER_PIXEL * (((int) x) + ((int) y) * WIDTH) + 1] = 255;
+        image[BYTES_PER_PIXEL * (((int) x) + ((int) y) * WIDTH) + 2] = 255;
     }
 }
 
@@ -179,28 +174,37 @@ void update_image (float *agents, uint8_t *image) {
         if (down > HEIGHT - 1) down = HEIGHT - 1;
         for (int ix = left; ix <= right; ix++) {
             for (int iy = up; iy <= down; iy++) {
-                total += ((float) image[ix + iy * WIDTH]) * KERNEL[iy-y+KERNEL_R][ix-x+KERNEL_R];
+                total += ((float) image[BYTES_PER_PIXEL * (ix + iy * WIDTH)]) * KERNEL[iy-y+KERNEL_R][ix-x+KERNEL_R];
                 count++;
             }
         }
-        image[i] = (int) (total * DECAY);
+        if (total >= DECAY) {
+            image[BYTES_PER_PIXEL * i] = (int) (total - DECAY);
+            image[BYTES_PER_PIXEL * i + 1] = (int) (total - DECAY);
+            image[BYTES_PER_PIXEL * i + 2] = (int) (total - DECAY);
+        }
+        else {
+            image[BYTES_PER_PIXEL * i] = 0;
+            image[BYTES_PER_PIXEL * i + 1] = 0;
+            image[BYTES_PER_PIXEL * i + 2] = 0;
+        }
     }
 }
 
 __global__
 void update_surface(cudaSurfaceObject_t cuda_surface, uint8_t *image) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < PIXELS) {
-        int x = i % WIDTH;
-        int y = i / WIDTH;
+    if (i < PIXELS_SIZE) {
+        int x = i % (BYTES_PER_PIXEL * WIDTH);
+        int y = i / (BYTES_PER_PIXEL * WIDTH);
         surf2Dwrite<uint8_t>(image[i], cuda_surface, x, y);
     }
 }
 
 void invokeRenderingKernel(cudaSurfaceObject_t cuda_surface) {
-    update_image<<<PIXELS/10, 10>>>(agents, image);
-    update_agents<<<NUM_AGENTS/10, 10>>>(agents, image);
-    update_surface<<<PIXELS/10, 10>>>(cuda_surface, image);
+    update_image<<<PIXELS_SIZE/PIXEL_DIV, PIXEL_DIV>>>(agents, image);
+    update_agents<<<NUM_AGENTS/AGENTS_DIV, AGENTS_DIV>>>(agents, image);
+    update_surface<<<PIXELS_SIZE/PIXEL_DIV, PIXEL_DIV>>>(cuda_surface, image);
 }
 
 void initializeGL () {
@@ -209,7 +213,7 @@ void initializeGL () {
     glBindTexture(GL_TEXTURE_2D, tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, WIDTH, HEIGHT, 0, GL_RED, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glBindTexture(GL_TEXTURE_2D, 0);
     cudaGraphicsGLRegisterImage(&cuda_resource, tex, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsWriteDiscard);
 }
@@ -262,39 +266,31 @@ int main (int argc, char *argv[]) {
     int i;
     
     host_agents = (float *) malloc(AGENTS_ARRAY_SIZE * sizeof(float));
-    host_image = (uint8_t *) malloc(PIXELS * sizeof(uint8_t));
+    host_image = (uint8_t *) malloc(PIXELS_SIZE * sizeof(uint8_t));
 
     cudaMalloc(&agents, AGENTS_ARRAY_SIZE * sizeof(float));
-    cudaMalloc(&image, PIXELS * sizeof(uint8_t));
+    cudaMalloc(&image, PIXELS_SIZE * sizeof(uint8_t));
 
     float angle;
     for (i = 0; i < NUM_AGENTS; i++) {
+
         host_agents[3 * i] = (float) (rand() % WIDTH);
         host_agents[3 * i + 1] = (float) (rand() % HEIGHT);
+
         angle = (float) (rand() % 360);
         angle *= M_PI/180.0f;
+
         host_agents[3 * i + 2] = angle;
     }
 
-    for (i = 0; i < PIXELS; i++) {
+    for (i = 0; i < PIXELS_SIZE; i++) {
         host_image[i] = 0;
     }
 
     cudaMemcpy(agents, host_agents, AGENTS_ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(image, host_image, PIXELS * sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(image, host_image, PIXELS_SIZE * sizeof(uint8_t), cudaMemcpyHostToDevice);
 
     glutMainLoop(); 
     
-    for (i = 0; i < TOTAL_STEPS; i++) {
-        update_image<<<PIXELS/100, 100>>>(agents, image);
-        update_agents<<<NUM_AGENTS/100, 100>>>(agents, image);
-
-        if (i % 1000 == 0) printf("%d\n", i);
-    }
-
-    //cudaMemcpy(host_agents, agents, AGENTS_ARRAY_SIZE * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(host_image, image, PIXELS * sizeof(uint8_t), cudaMemcpyDeviceToHost);
-    stbi_write_png("output.png", WIDTH, HEIGHT, 1, host_image, WIDTH * 1);
-
     return 0;
 }
